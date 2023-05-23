1a0865b8bff289dd6f7616e204a853d1bcda1d1f
helenebundgaard <98382836+helenebundgaard@users.noreply.github.com>
2023-05-23 19:18:00 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,31 @@
+
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+
+const el = document.getElementById('button');
+
+setTimeout(() => {
+  el.style.visibility = 'visible';
+
+  // 👇️ if you used `display` to hide the element
+  // el.style.display = 'block';
+}, 35000); // 👈️ delay in milliseconds
